16e52e7c39d5d72ac94e38bcd596b04a45679f5e
Datzz <74682589+dtzdev@users.noreply.github.com>
2023-04-18 09:01:30 +0300

    EI vitunkaa hajua toimiiko mut pitäs
    En jaksa buildaa kirjotin vaa nopee koulus testaa ite ja heitä database osote connection stringi

--- Program.cs
+++ Program.cs
@@ -72,6 +72,27 @@ namespace Tuntikirjanpitosovellus
         private static void SaveTimeEntry(string username, string project, string description, double hours, DateTime date)
         {
             // Tallenna tuntimerkintä tietokantaan
+                    // Yhdistä tietokantaan
+    string connectionString = "CONNECTION_STRING_TAHA";
+    using (SqlConnection connection = new SqlConnection(connectionString))
+    {
+        connection.Open();
+
+        // Luo SQL req tallentaaksesi tuntimerkintä tietokantaan
+        string query = "INSERT INTO TimeEntries (Username, Project, Description, Hours, Date) VALUES (@Username, @Project, @Description, @Hours, @Date)";
+        using (SqlCommand command = new SqlCommand(query, connection))
+        {
+            // Aseta SQL parametrit
+            command.Parameters.AddWithValue("@Username", username);
+            command.Parameters.AddWithValue("@Project", project);
+            command.Parameters.AddWithValue("@Description", description);
+            command.Parameters.AddWithValue("@Hours", hours);
+            command.Parameters.AddWithValue("@Date", date);
+
+            // Suorita SQL
+            command.ExecuteNonQuery();
+        }
+    }
         }
     }
 }
